5273c0cf39db93b5e2247ec4b0ceb0a4cc6f322d
lucashflores <lucas.henrique.flores2001@gmail.com>
2023-05-01 11:14:19 -0300

    jump done

--- control_unit.vhd
+++ control_unit.vhd
@@ -38,27 +38,29 @@ architecture a_control_unit of control_unit is
         ); 
     end component;
 
+    signal pc_count: unsigned(6 downto 0) := "0000000";
     signal pc_address_in : unsigned(6 downto 0) := "0000000";
     signal pc_address_out: unsigned(6 downto 0);
     signal state: std_logic := '0';
     signal wr_en: std_logic := '0';
-    -- signal jump_en: std_logic := '0';
-    -- signal jump_address: unsigned(6 downto 0) := "0000000";
-    -- signal opcode: unsigned(3 downto 0) := "0000";
-    -- signal rom_data: unsigned(13 downto 0) := "00000000000000";
+    signal jump_en: std_logic := '0';
+    signal jump_address: unsigned(6 downto 0) := "0000000";
+    signal opcode: unsigned(3 downto 0) := "0000";
+    signal rom_data: unsigned(13 downto 0) := "00000000000000";
 begin
     pc_instance: program_counter port map(clk => clk, wr_en => wr_en, rst => rst, address_in => pc_address_in, address_out => pc_address_out);
 
     state_machine_instance: state_machine port map(clk => clk, rst => rst, current_state => state);
 
-    rom_instance: rom port map(clk => clk, address => pc_address_out, data => data);
+    rom_instance: rom port map(clk => clk, address => pc_address_out, data => rom_data);
 
-    -- data <= rom_data;
+    data <= rom_data;
     wr_en <= '1' when state = '1' else '0';
-    -- opcode <= rom_data(13 downto 10);
-    -- jump_en <= '1' when opcode = "1111" else '0';
-    -- jump_address <= rom_data(6 downto 0);
-    pc_address_in <= pc_address_out + 1 when state = '1' else pc_address_out + 0;
+    opcode <= rom_data(13 downto 10);
+    jump_en <= '1' when opcode = "1111" else '0';
+    jump_address <= rom_data(6 downto 0);
+    pc_count <= pc_address_out + 1 when state = '1' else pc_address_out;
+    pc_address_in <= jump_address when jump_en = '1' else pc_count;
     current_state <= state;                
 end architecture;
 
